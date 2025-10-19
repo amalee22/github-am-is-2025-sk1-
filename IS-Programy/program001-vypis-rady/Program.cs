c84fd3f1149f580a31ba75f0e29e97a8a73f91ce
Amálie Musilová <amusilova222@gmail.com>
2025-10-19 20:53:24 +0200

    Úkol 003 - první program v C#  Fixes #2

---
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Numerics;
+
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("****************************");
+    Console.WriteLine("***** Výpis řady čísel *****");
+    Console.WriteLine("****************************");
+    Console.WriteLine("***** Amálie Musilová ******");
+    Console.WriteLine("****************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu - špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    //Vstup hodnoty do programu - řešený správně
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+    }
+
+    Console.Write("Zadejte poslední číslo řady (celé číslo): ");
+    int last;
+    while (!int.TryParse(Console.ReadLine(), out last))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte poslední číslo řady znovu: ");
+    }
+
+    Console.Write("Zadejte diferenci (celé číslo): ");
+    int step;
+    while (!int.TryParse(Console.ReadLine(), out step))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte diferenci znovu: ");
+    }
+
+    // Výpis vstupních hodnot
+    Console.WriteLine();
+    Console.WriteLine("====================================");
+    Console.WriteLine("Zadali jste tyto hodnoty: ");
+    Console.WriteLine("První číslo řady: {0}", first);
+    Console.WriteLine("Poslední číslo řady: {0}", last);
+    Console.WriteLine("Diference: {0}", step);
+    Console.WriteLine("====================================");
+    Console.WriteLine("První číslo: {0}; Poslední číslo: {1}; Diference: {2}", first, last, step);
+    Console.WriteLine("====================================");
+    Console.WriteLine();
+
+    // Výpis řady
+    Console.WriteLine();
+    Console.WriteLine("====================================");
+    Console.WriteLine("Výpis číselné řady:");
+    int current = first;
+    while (current <= last) {
+        Console.WriteLine(current);
+        current = current + step; // ruční přičtení diference
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+    again = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+}
