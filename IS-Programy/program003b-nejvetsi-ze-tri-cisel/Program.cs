eef35c1848d5a473240702a84894c4422222526d
Amálie Musilová <amusilova222@gmail.com>
2025-10-22 10:01:07 +0200

    #5 největší ze tří čísel

--- IS-Programy/program003b-nejvetsi-ze-tri-cisel/Program.cs
+++ IS-Programy/program003b-nejvetsi-ze-tri-cisel/Program.cs
@@ -1,2 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http.Headers;
+using System.Numerics;
+
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("******************************");
+    Console.WriteLine("***** Největší ze tří čísel *****");
+    Console.WriteLine("******************************");
+    Console.WriteLine("****** Amálie Musilová *******");
+    Console.WriteLine("******************************");
+    Console.WriteLine();
+
+
+    //Vstup hodnoty A
+    Console.Write("Zadejte celé číslo - hodnota A: ");
+    int a;
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu A znovu: ");
+    }
+
+
+//Vstup hodnoty B
+    Console.Write("Zadejte celé číslo - hodnota B: ");
+    int b;
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu B znovu: ");
+    }
+
+
+    //Vstup hodnoty C
+    Console.Write("Zadejte celé číslo - hodnota C: ");
+    int c;
+    while (!int.TryParse(Console.ReadLine(), out c))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu C znovu: ");
+    }
+
+    Console.WriteLine("===================================================");
+
+    if (a > b)
+    {
+        if (a > c)
+        { //největší číslo je a a = hodnota a
+            Console.WriteLine($"Největší číslo je A= {a}");
+        }
+        else
+        {
+            Console.WriteLine($"Největší číslo je C= {c}");
+        }
+    }
+
+    else
+    {
+        if (b > c)
+            Console.WriteLine($"Největší číslo je B= {b}");
+        else
+        {
+            Console.WriteLine($"Největší číslo je C= {c}");
+        }
+    }
+
+
+Console.WriteLine("===================================================");
+
+
+
+   
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+
+}
+
+
+
--- IS-Programy/program003b-nejvetsi-ze-tri-cisel/obj/Debug/net9.0/program003b-nejvetsi-ze-tri-cisel.AssemblyInfo.cs
+++ IS-Programy/program003b-nejvetsi-ze-tri-cisel/obj/Debug/net9.0/program003b-nejvetsi-ze-tri-cisel.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("program003a-serazeni-dvou-cisel")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("program003b-nejvetsi-ze-tri-cisel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68c85e04d38e3ab16affc29ee2aee6b116ba6f78")]
-[assembly: System.Reflection.AssemblyProductAttribute("program003a-serazeni-dvou-cisel")]
-[assembly: System.Reflection.AssemblyTitleAttribute("program003a-serazeni-dvou-cisel")]
+[assembly: System.Reflection.AssemblyProductAttribute("program003b-nejvetsi-ze-tri-cisel")]
+[assembly: System.Reflection.AssemblyTitleAttribute("program003b-nejvetsi-ze-tri-cisel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Vygenerované třídou WriteCodeFragment nástroje MSBuild
