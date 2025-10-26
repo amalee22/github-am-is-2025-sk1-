c94b4f54ca9c375bc367a83d75d7b1507133f461
Amálie Musilová <amusilova222@gmail.com>
2025-10-26 15:22:45 +0100

    #7 vykreslen druhý obrazec

--- IS-Programy/program004c-obrazec10/Program.cs
+++ IS-Programy/program004c-obrazec10/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************");
+    Console.WriteLine("**** Vykreslení obrazce 10 ****");
+    Console.WriteLine("********************************");
+    Console.WriteLine("******** Amálie Musilová *******");
+    Console.WriteLine("********************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte výšku obrazce (celé číslo): ");
+    int size;
+
+    while (!int.TryParse(Console.ReadLine(), out size))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte výšku znovu: ");
+    }
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write("* ");
+        }
+        else
+        {
+            if (i % 4 == 1 && j == size - 1)
+                Console.Write("*");
+            else if (i % 4 == 3 && j == 0)
+                Console.Write("*");
+            else
+                Console.Write("  ");
+        }
+    }
+    Console.WriteLine();
+    System.Threading.Thread.Sleep(50);
+}
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine() ?? "";
+}
