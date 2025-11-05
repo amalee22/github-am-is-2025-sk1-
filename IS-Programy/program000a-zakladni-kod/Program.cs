e18a1f887219fa8044f945dd05abcd22933819d4
Amálie Musilová <amusilova222@gmail.com>
2025-11-05 09:50:26 +0100

    "něco"

--- IS-Programy/program000a-zakladni-kod/Program.cs
+++ IS-Programy/program000a-zakladni-kod/Program.cs
@@ -1,13 +1,11 @@
-﻿using System.Numerics;
-
-string again = "a";
+﻿﻿string again = "a";
 while (again == "a")
 {
     Console.Clear();
     Console.WriteLine("****************************");
-    Console.WriteLine("***** Výpis řady čísel *****");
+    Console.WriteLine("***** Název programu **********");
     Console.WriteLine("****************************");
-    Console.WriteLine("****** Amálie Musilová *****");
+    Console.WriteLine("******* Amálie Musilová ********");
     Console.WriteLine("****************************");
     Console.WriteLine();
 
@@ -16,54 +14,22 @@ while (again == "a")
     //int first = int.Parse(Console.ReadLine());
 
     //Vstup hodnoty do programu - řešený správně
-    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    Console.Write("Zadejte hodnotu (celé číslo): ");
     int first;
+
     while (!int.TryParse(Console.ReadLine(), out first))
     {
-        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu znovu: ");
     }
 
-    Console.Write("Zadejte poslední číslo řady (celé číslo): ");
-    int last;
-    while (!int.TryParse(Console.ReadLine(), out last))
-    {
-        Console.Write("Nezadali jste celé číslo. Zadejte poslední číslo řady znovu: ");
-    }
-
-    Console.Write("Zadejte diferenci (celé číslo): ");
-    int step;
-    while (!int.TryParse(Console.ReadLine(), out step))
-    {
-        Console.Write("Nezadali jste celé číslo. Zadejte diferenci znovu: ");
-    }
-
-    // Výpis vstupních hodnot
-    Console.WriteLine();
-    Console.WriteLine("====================================");
-    Console.WriteLine("Zadali jste tyto hodnoty: ");
-    Console.WriteLine("První číslo řady: {0}", first);
-    Console.WriteLine("Poslední číslo řady: {0}", last);
-    Console.WriteLine("Diference: {0}", step);
-    Console.WriteLine("====================================");
-    Console.WriteLine("První číslo: {0}; Poslední číslo: {1}; Diference: {2}", first, last, step);
-    Console.WriteLine("====================================");
-    Console.WriteLine();
-
-    // Výpis řady
-    Console.WriteLine();
-    Console.WriteLine("====================================");
-    Console.WriteLine("Výpis číselné řady:");
-    int current = first;
-    while (current <= last) {
-        Console.WriteLine(current);
-        current = current + step; // ruční přičtení diference
-    }
 
     Console.WriteLine();
     Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+
     again = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+
 
 }
-
-
-
---
+++ IS-Programy/program005-generator/Program.cs
@@ -0,0 +1,87 @@
+﻿﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("************************************************");
+    Console.WriteLine("***** Generátor pseudonáhodných čísel **********");
+    Console.WriteLine("************************************************");
+    Console.WriteLine("************** Amálie Musilová *****************");
+    Console.WriteLine("************************************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu - špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    //Vstup hodnoty do programu - řešený správně
+    Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
+    int n; //proměnná pro počet čísel n 
+
+    while (!int.TryParse(Console.ReadLine(), out n))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte znovu počet čísel: ");
+    }
+
+//DOLNÍ MEZ 
+Console.Write("Zadejte dolní mez (celé číslo): ");
+    int lowerBound;
+
+    while (!int.TryParse(Console.ReadLine(), out lowerBound))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte znovu dolní mez: ");
+    }
+
+
+//HORNÍ MEZ
+    Console.Write("Zadejte horní mez (celé číslo): ");
+    int upperBound;
+
+    while (!int.TryParse(Console.ReadLine(), out upperBound))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte znovu horní mez: ");
+    }
+    
+
+Console.WriteLine();
+Console.WriteLine("======================================================");
+Console.WriteLine("Zadané hodnoty: "); //vypsání zadaných hodnot 
+Console.WriteLine($"Počet generovaných čísel: {0}; Dolní mez :{1}; Horní mez: {2}", n, lowerBound, upperBound);
+Console.WriteLine("======================================================");
+
+
+    //DEKLARACE POLE
+    // jen jmenování mé proměnné  ,  instance třídy int - nadeklarování pole celých čísel  
+    int[] myRandomNumbers = new int[n];
+
+
+    //vytvoření objektu  - POZOR- JEN NUMBER NE S - mn.č. /generování stejných čísel při stejném vstupu - hodí se pro TESTOVÁNÍ 
+    Random myRandomNumber = new Random();
+
+    Console.WriteLine();
+    Console.WriteLine("======================================================");
+    Console.WriteLine("Pseudonáhodná čísla:  ");
+
+
+    //cyklus  - jde také napsat pomocí WHILE 
+for(int i = 0; i < n ; i ++)
+        { //metoda .Next - 2 parametry - dolní a horní mez
+        myRandomNumbers[i] = myRandomNumber.Next(lowerBound, upperBound);
+        Console.WriteLine("{0}; ", myRandomNumbers[i]); 
+        
+        //i se zvětší na i++ = 1
+    }
+
+
+    Console.WriteLine("======================================================");
+
+
+Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+
+    again = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+
+
+}
